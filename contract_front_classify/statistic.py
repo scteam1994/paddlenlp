102749056ada5ddbb730a1b76b64a1ddc84c600e
scteam1994 <scteam1994@gmail.com>
2023-08-10 20:11:38 +0800

    optimal model

--- contract_front_classify/statistic.py
+++ contract_front_classify/statistic.py
@@ -1,6 +1,7 @@
 import glob
 import json
 import os
+import random
 import re
 from sklearn import svm
 import numpy as np
@@ -127,20 +128,17 @@ def generate_data(keywords, words_all):
                 for i, keyword in enumerate(keywords):
                     if isinstance(keyword, str):
                         if keyword in word:
-                            contains[i] = 1
+                            contains[i] = word.count(keyword)
                     elif isinstance(keyword, list):
                         for kw in keyword:
                             if kw in word:
-                                contains[i] = 1
+                                contains[i] = word.count(keyword)
         elif isinstance(head_words, str):
             for i, keyword in enumerate(keywords):
                 if isinstance(keyword, str):
                     if keyword in head_words:
-                        contains[i] = 1
+                        contains[i] = head_words.count(keyword)
                 elif isinstance(keyword, list):
-                    # for kw in keyword:
-                    #     if kw in verf_data:
-                    #         contains[i] = 1
                     if "all" == keyword[0]:
                         verf_data = head_words
                         for kw in keyword[1:]:
@@ -153,11 +151,11 @@ def generate_data(keywords, words_all):
                         verf_data = head_words[:keyword[0]]
                         for kw in keyword[1:]:
                             if kw in verf_data:
-                                contains[i] = 1
+                                contains[i] = verf_data.count(kw)
                     else:
                         for kw in keyword:
                             if kw in head_words:
-                                contains[i] = 1
+                                contains[i] = head_words.count(kw)
         contains_data.append(np.hstack((contains,np.array(loc))))
     return contains_data
 
@@ -165,7 +163,7 @@ def generate_data(keywords, words_all):
 def convert_data_to_logits(keywords, data):
     words_all = list(data.values())
     words_all_flatten = [flatten_txt(words_all[i]) for i in range(len(words_all))]
-    words_frq = frequency_count(words_all_flatten)
+    # words_frq = frequency_count(words_all_flatten)
     data = generate_data(keywords, words_all_flatten)
     data = np.array(data)
     return data
@@ -212,59 +210,32 @@ def convert_data_to_datasets(keywords, data, label_value):
     label = np.ones(len(logits_data)) * label_value
     return logits_data, label
 
-
+keywords = [
+    [['all'],'销售合同','供货合同','购销合同','承揽合同','销购合同','采购协议','供应合同','定做合同','加工合同'],
+    [['all'],'甲方', '乙方', '买方', '卖方'],
+    [['all'],'发包', '承包', '全称', '承租', '出租', '分包', '建设单位', '施工单位','转让方','受让方','转让人','受让人','出卖','买受','供方','需方','委托人','代建人','委托方','代建方'],
+    [['all'],'编号','地点','时间','签订'],
+    [['all'],'(以下简称', '（以下简称'],
+    [['all'],'第一部分', '第一条', '第一节', '第一章'],
+    [['all'],'一、', '一。', '一．', '一.'],
+    [['all'], '协议'],
+    [['all'], '项目合同书', '采购合同', '销售合同', '买卖合同', '工程合同', '施工合同','合同条款','分包合同','安装合同','服务合同','维护合同','承包合同'],
+    [['all'],'概况', '概述'],
+    [['all'],'(盖章', '(签字', '(公章', '(盖单位章','（盖章', '（签字', '（公章','（盖单位章', '盖章)', '签字)', '公章)','盖单位章)', '盖章）', '签字）', '公章）''盖单位章）','(签名','（签名','签名）','签名)','(印章','（印章','印章）','印章)',],
+    [['all'],'附件','附录','目录','说明'],
+    [['all'],'中华人民共和国'],
+    [['all'],'有限公司'],
+    [['all'],'代理','委托'],
+    [['all'],'法定代表'],
+    [['all'],'补充'],
+    [['all'],'自愿','公平'],
+    [['all'],'有限公司'],
+]
 if __name__ == '__main__':
     root = '/home/topnet/图片'
     folders = ['head2', 'tail', 'other']
-    keywords = [
-        [[20, 50, 100,'all'], '销售合同', '供货合同', '购销合同', '承揽合同', '销购合同', '采购协议', '供应合同',
-         '定做合同', '加工合同'],
-        [[20, 70, 'all'], '协议'],
-        [[20, 80,'all'], '项目合同书', '采购合同', '销售合同', '买卖合同', '工程合同', '施工合同', '合同条款',
-         '分包合同', '安装合同', '服务合同', '维护合同', '承包合同'],
-        [['all'],'甲方', '乙方','买方', '卖方',],
-        [[50,200], '发包', '承包', '全称', '承租', '出租', '分包', '建设单位',
-         '施工单位', '转让方', '受让方', '转让人', '受让人', '出卖', '买受', '供方', '需方', '委托人', '代建人',
-         '委托方', '代建方'],
-        [[50, 200], '编号', '地点', '时间', '签订'],
-        [['all'], '(以下简称', '（以下简称'],
-        [[50,'all'], '第一', '第一部分', '第一条', '第一节', '第一章'],
-        [['all'], '一、', '一。', '一．', '一.'],
 
-        [['all'], '概况', '概述'],
-        [['all'], '(盖章', '(签字', '(公章', '(盖单位章', '（盖章', '（签字', '（公章', '（盖单位章', '盖章)', '签字)',
-         '公章)', '盖单位章)', '盖章）', '签字）', '公章）''盖单位章）', '(签名', '（签名', '签名）', '签名)', '(印章',
-         '（印章', '印章）', '印章)', ],
-        [[10, 20, 'all'], '附件', '附录', '目录', '说明'],
-        [['all'], '中华人民共和国'],
-        [['all'], '有限公司'],
-        [['all'], '代理', '委托'],
-        [['all'], '法定代表'],
-        [['all'], '补充'],
-        [['all'], '自愿', '公平'],
-        [['all'], '有限公司'],
-    ]
-    keywords = [
-        [['all'],'销售合同','供货合同','购销合同','承揽合同','销购合同','采购协议','供应合同','定做合同','加工合同'],
-        [['all'],'甲方', '乙方', '买方', '卖方'],
-        [['all'],'发包', '承包', '全称', '承租', '出租', '分包', '建设单位', '施工单位','转让方','受让方','转让人','受让人','出卖','买受','供方','需方','委托人','代建人','委托方','代建方'],
-        [['all'], '编号','地点','时间','签订'],
-        [['all'],'(以下简称', '（以下简称'],
-        [['all'],'第一部分', '第一条', '第一节', '第一章'],
-        [['all'],'一、', '一。', '一．', '一.'],
-        [['all'], '协议'],
-        [['all'], '项目合同书', '采购合同', '销售合同', '买卖合同', '工程合同', '施工合同','合同条款','分包合同','安装合同','服务合同','维护合同','承包合同'],
-        [['all'],'概况', '概述'],
-        [['all'],'(盖章', '(签字', '(公章', '(盖单位章','（盖章', '（签字', '（公章','（盖单位章', '盖章)', '签字)', '公章)','盖单位章)', '盖章）', '签字）', '公章）''盖单位章）','(签名','（签名','签名）','签名)','(印章','（印章','印章）','印章)',],
-        [['all'],'附件','附录','目录','说明'],
-        [['all'],'中华人民共和国'],
-        [['all'],'有限公司'],
-        [['all'],'代理','委托'],
-        [['all'],'法定代表'],
-        [['all'],'补充'],
-        [['all'],'自愿','公平'],
-        [['all'],'有限公司'],
-    ]
+
     keywords_flatten = []
     for i in keywords:
         word_limit = i[0]
@@ -289,64 +260,70 @@ if __name__ == '__main__':
     r = r"[|_|.|!|+|-|=|—|,|$|￥|%|^|，|。|？|、|~|@|#|￥|%|…|&|*|《|》|<|>|「|」|{|}|【|】|(|)|/|]|:|：|；|‘|’|“|”|,|（|）"
     ([head, tail, other], [head_img, tail_img, other_img]) = read_image_txt(root, folders, img_size, use_img=False)
     head_data, head_label = convert_data_to_datasets(keywords_flatten, head, 1)
-    tail_data, tail_label = convert_data_to_datasets(keywords_flatten, tail, 0)
+    tail_data, tail_label = convert_data_to_datasets(keywords_flatten, tail, 2)
     other_data, other_label = convert_data_to_datasets(keywords_flatten, other, 0)
     text_input = np.concatenate((head_data, tail_data, other_data), axis=0)
     img_input = np.concatenate((head_img, tail_img, other_img), axis=0)
     label = np.concatenate((head_label, tail_label, other_label), axis=0)
     img_name = list(head.keys()) + list(tail.keys()) + list(other.keys())
     # tran test split
-    x1_train, x1_test, x2_train, x2_test, y_train_raw, y_test_raw, img_name_train, img_name_test = train_test_split(text_input, img_input, label,img_name,
-                                                                                     random_state=113, train_size=0.7)
-    if tensorflow_backend:
-        # tensorflow mix model
-        import tensorflow as tf
-        y_train = tf.one_hot(y_train_raw,output_shape)
-        y_test = tf.one_hot(y_test_raw,output_shape)
+    acc = []
+    for _ in range(20):
+        seed = random.randint(0, 1000)
+        x1_train, x1_test, x2_train, x2_test, y_train_raw, y_test_raw, img_name_train, img_name_test = train_test_split(text_input, img_input, label,img_name,
+                                                                                         random_state=seed, train_size=0.7)
+        if tensorflow_backend:
+            # tensorflow mix model
+            import tensorflow as tf
+            y_train = tf.one_hot(y_train_raw,output_shape)
+            y_test = tf.one_hot(y_test_raw,output_shape)
 
-        # model = get_model_x(input_shape1=(len(keywords_flatten),),input_shape2=(img_size,img_size,3),output_shape)
-        # model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-        # model.fit([x1_train,x2_train], y_train, epochs=100, batch_size=32,validation_data=([x1_test,x2_test],y_test),callbacks=[tf.keras.callbacks.ModelCheckpoint('./contract_models/model.h5',save_best_only=True)])
+            # model = get_model_x(input_shape1=(len(keywords_flatten),),input_shape2=(img_size,img_size,3),output_shape)
+            # model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+            # model.fit([x1_train,x2_train], y_train, epochs=100, batch_size=32,validation_data=([x1_test,x2_test],y_test),callbacks=[tf.keras.callbacks.ModelCheckpoint('./contract_models/model.h5',save_best_only=True)])
 
-        model = get_model_txt(input_shape1= (x1_train.shape[1],1)
-                              ,output_shape=output_shape)
-        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-        model.summary()
-        model.fit(x1_train[:,:,np.newaxis], y_train, epochs=100, batch_size=32,validation_data=(x1_test[:,:,np.newaxis],y_test),callbacks=[tf.keras.callbacks.ModelCheckpoint('./contract_models/model.h5',save_best_only=True)])
+            model = get_model_txt(input_shape1= (x1_train.shape[1],1)
+                                  ,output_shape=output_shape)
+            model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+            model.summary()
+            model.fit(x1_train[:,:,np.newaxis], y_train, epochs=100, batch_size=32,validation_data=(x1_test[:,:,np.newaxis],y_test),callbacks=[tf.keras.callbacks.ModelCheckpoint('./contract_models/model.h5',save_best_only=True)])
 
-    # 'sigmoid', 'poly', 'precomputed', 'rbf', 'linear'
-    # 'ovr', 'ovo'
-    if svm_backend:
-        clf = svm.SVC(C=0.85, kernel='linear', decision_function_shape='ovr', verbose=True)
-        clf.fit(x1_train, y_train_raw)
-        y_pred = clf.predict(x1_test)
-        y_pred[y_pred == 2] = 0
-        y_test_raw[y_test_raw == 2] = 0
-        y_pred_train = clf.predict(x1_train)
-        y_pred_train[y_pred_train == 2] = 0
-        y_train_raw[y_train_raw == 2] = 0
+        # 'sigmoid', 'poly', 'precomputed', 'rbf', 'linear'
+        # 'ovr', 'ovo'
+        if svm_backend:
+            clf = svm.SVC(C=0.85, kernel='linear', decision_function_shape='ovr', verbose=False)
+            clf.fit(x1_train, y_train_raw)
+            y_pred = clf.predict(x1_test)
+            y_pred[y_pred == 2] = 0
+            y_test_raw[y_test_raw == 2] = 0
+            y_pred_train = clf.predict(x1_train)
+            y_pred_train[y_pred_train == 2] = 0
+            y_train_raw[y_train_raw == 2] = 0
 
-        for i in np.where(y_test_raw != y_pred)[0]:
-            print(img_name_test[i])
-            img_path = glob.glob(os.path.join(root, '*', img_name_test[i]))[0]
-            img = cv2.imread(img_path)
-            label = img_path.split('/')[-2]
-            # cv2.imshow(f'{label}', img)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
+            # for i in np.where(y_test_raw != y_pred)[0]:
+            #     print(f"'{img_name_test[i]}'")
+            #     img_path = glob.glob(os.path.join(root, '*', img_name_test[i]))[0]
+            #     img = cv2.imread(img_path)
+            #     label = img_path.split('/')[-2]
+                # cv2.imshow(f'{label}', img)
+                # cv2.waitKey(0)
+                # cv2.destroyAllWindows()
 
-        print('-------------------')
-        for i in np.where(y_train_raw != y_pred_train)[0]:
-            print(img_name_train[i])
-            img_path = glob.glob(os.path.join(root, '*', img_name_train[i]))[0]
-            img = cv2.imread(img_path)
-            label = img_path.split('/')[-2]
-            # cv2.imshow(f'{label}', img)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
-        print('all test num : ', len(y_test_raw))
-        print(accuracy_score(y_test_raw, y_pred))
+            # print('-------------------')
+            # for i in np.where(y_train_raw != y_pred_train)[0]:
+                # print(img_name_train[i])
+                # img_path = glob.glob(os.path.join(root, '*', img_name_train[i]))[0]
+                # img = cv2.imread(img_path)
+                # label = img_path.split('/')[-2]
+                # cv2.imshow(f'{label}', img)
+                # cv2.waitKey(0)
+                # cv2.destroyAllWindows()
+            # print('all test num : ', len(y_test_raw))
+            acc.append(accuracy_score(y_test_raw, y_pred))
+            # print(accuracy_score(y_test_raw, y_pred))
 
-        print(accuracy_score(y_train_raw, y_pred_train))
-        with open('contract_front_classify/model_loc.pkl', 'wb') as f:
-            pickle.dump(clf, f)
+            # print(accuracy_score(y_train_raw, y_pred_train))
+            with open('contract_front_classify/model_loc.pkl', 'wb') as f:
+                pickle.dump(clf, f)
+    print(np.mean(acc))
+
--- contract_front_classify/svm_infer.py
+++ contract_front_classify/svm_infer.py
@@ -6,59 +6,28 @@ import cv2
 import numpy as np
 from paddleocr import PaddleOCR
 
-
+from statistic import keywords
 class Contract_front_classifier(object):
     def __init__(self,flatten=True):
-        self.keywords =[
-        [[20, 50, 100,'all'], '销售合同', '供货合同', '购销合同', '承揽合同', '销购合同', '采购协议', '供应合同',
-         '定做合同', '加工合同'],
-        [[20, 70, 'all'], '协议'],
-        [[20, 80,'all'], '项目合同书', '采购合同', '销售合同', '买卖合同', '工程合同', '施工合同', '合同条款',
-         '分包合同', '安装合同', '服务合同', '维护合同', '承包合同'],
-        [['all'],'甲方', '乙方','买方', '卖方',],
-        [[50,200], '发包', '承包', '全称', '承租', '出租', '分包', '建设单位',
-         '施工单位', '转让方', '受让方', '转让人', '受让人', '出卖', '买受', '供方', '需方', '委托人', '代建人',
-         '委托方', '代建方'],
-        [[50, 200], '编号', '地点', '时间', '签订'],
-        [['all'], '(以下简称', '（以下简称'],
-        [[50,'all'], '第一', '第一部分', '第一条', '第一节', '第一章'],
-        [['all'], '一、', '一。', '一．', '一.'],
-
-        [['all'], '概况', '概述'],
-        [['all'], '(盖章', '(签字', '(公章', '(盖单位章', '（盖章', '（签字', '（公章', '（盖单位章', '盖章)', '签字)',
-         '公章)', '盖单位章)', '盖章）', '签字）', '公章）''盖单位章）', '(签名', '（签名', '签名）', '签名)', '(印章',
-         '（印章', '印章）', '印章)', ],
-        [[10, 20, 'all'], '附件', '附录', '目录', '说明'],
-        [['all'], '中华人民共和国'],
-        [['all'], '有限公司'],
-        [['all'], '代理', '委托'],
-        [['all'], '法定代表'],
-        [['all'], '补充'],
-        [['all'], '自愿', '公平'],
-        [['all'], '有限公司'],
-    ]
-        self.keywords = [
-        [['all'],'销售合同','供货合同','购销合同','承揽合同','销购合同','采购协议','供应合同','定做合同','加工合同'],
-        [['all'],'甲方', '乙方', '买方', '卖方'],
-        [['all'], '发包', '承包', '全称', '承租', '出租', '分包', '建设单位', '施工单位', '转让方', '受让方',
-         '转让人', '受让人', '出卖', '买受', '供方', '需方', '委托人', '代建人', '委托方', '代建方'],
-        [['all'], '编号','地点','时间','签订'],
-        [['all'],'(以下简称', '（以下简称'],
-        [['all'],'第一部分', '第一条', '第一节', '第一章'],
-        [['all'],'一、', '一。', '一．', '一.'],
-        [['all'], '协议'],
-        [['all'], '项目合同书', '采购合同', '销售合同', '买卖合同', '工程合同', '施工合同','合同条款','分包合同','安装合同','服务合同','维护合同','承包合同'],
-        [['all'],'概况', '概述'],
-        [['all'],'(盖章', '(签字', '(公章', '(盖单位章','（盖章', '（签字', '（公章','（盖单位章', '盖章)', '签字)', '公章)','盖单位章)', '盖章）', '签字）', '公章）''盖单位章）','(签名','（签名','签名）','签名)','(印章','（印章','印章）','印章)',],
-        [['all'],'附件','附录','目录','说明'],
-        [['all'],'中华人民共和国'],
-        [['all'],'有限公司'],
-        [['all'],'代理','委托'],
-        [['all'],'法定代表'],
-        [['all'],'补充'],
-        [['all'],'自愿','公平'],
-        [['all'],'有限公司'],
-    ]
+    #     self.keywords = [
+    #     [['all'],'销售合同','供货合同','购销合同','承揽合同','销购合同','采购协议','供应合同','定做合同','加工合同'],
+    #     [['all'],'甲方', '乙方', '买方', '卖方'],
+    #     [['all'],'发包', '承包', '全称', '承租', '出租', '分包', '建设单位', '施工单位','转让方','受让方','转让人','受让人','出卖','买受','供方','需方','委托人','代建人','委托方','代建方','代理人','代理方',],
+    #     [['all'],'地点','时间','签订'],
+    #     [['all'],'(以下简称', '（以下简称'],
+    #     [['all'],'第一部分', '第一条', '第一节', '第一章'],
+    #     [['all'],'一、', '一。', '一．', '一.'],
+    #     [['all'], '协议'],
+    #     [['all'], '项目合同书', '采购合同', '销售合同', '买卖合同', '工程合同', '施工合同','合同条款','分包合同','安装合同','服务合同','维护合同','承包合同'],
+    #     [['all'],'概况', '概述'],
+    #     [['all'],'(盖章', '(签字', '(公章', '(盖单位章','（盖章', '（签字', '（公章','（盖单位章', '盖章)', '签字)', '公章)','盖单位章)', '盖章）', '签字）', '公章）''盖单位章）','(签名','（签名','签名）','签名)','(印章','（印章','印章）','印章)',],
+    #     [['all'],'附件','附录','目录','说明'],
+    #     [['all'],'中华人民共和国'],
+    #     [['all'],'法定代表'],
+    #     [['all'],'补充'],
+    #     [['all'],'自愿','公平'],
+    # ]
+        self.keywords = keywords
         self.keywords_flatten = []
         for i in self.keywords:
             word_limit = i[0]
@@ -104,17 +73,20 @@ class Contract_front_classifier(object):
                     for i,keyword in enumerate(self.keywords):
                         if isinstance(keyword,str):
                             if keyword in word:
-                                contains[i] = 1
-                        elif isinstance(keyword,list):
+                                contains[i] = word.count(keyword)
+                        elif isinstance(keyword, list):
                             for kw in keyword:
                                 if kw in word:
-                                    contains[i] = 1
-            elif isinstance(head_words,str):
-                for i,keyword in enumerate(self.keywords):
-                    if isinstance(keyword,str):
+                                    contains[i] = word.count(keyword)
+            elif isinstance(head_words, str):
+                for i, keyword in enumerate(self.keywords):
+                    if isinstance(keyword, str):
                         if keyword in head_words:
-                            contains[i] = 1
-                    elif isinstance(keyword,list):
+                            contains[i] = head_words.count(keyword)
+                    elif isinstance(keyword, list):
+                        # for kw in keyword:
+                        #     if kw in verf_data:
+                        #         contains[i] = 1
                         if "all" == keyword[0]:
                             verf_data = head_words
                             for kw in keyword[1:]:
@@ -127,12 +99,11 @@ class Contract_front_classifier(object):
                             verf_data = head_words[:keyword[0]]
                             for kw in keyword[1:]:
                                 if kw in verf_data:
-                                    contains[i] = 1
+                                    contains[i] = verf_data.count(kw)
                         else:
                             for kw in keyword:
                                 if kw in head_words:
-                                    contains[i] = 1
-            # contains_data.append(contains)
+                                    contains[i] = head_words.count(kw)
             contains_data.append(np.hstack((contains, np.array(loc))))
         return contains_data
     def flatten_txt(self,txt_list:list):
@@ -153,21 +124,39 @@ class Contract_front_classifier(object):
 model = Contract_front_classifier()
 ocr = PaddleOCR(use_angle_cls=True, lang="ch",use_gpu=True,ocr_version="PP-OCRv4")
 
-test_path = '/home/topnet/图片/house_sale_contract/1'
-ocr_result_dict = {}
+test_path = '/home/topnet/图片/house_sale_contract/3'
+test_path = '/home/topnet/图片/media'
+
+imglist = [
+    '2023072514_a61fd153d19e4e90ccb05eff641d4a6f.png' ,
+    'image108.jpeg'                                   ,
+    '6b494a8e8e64a949dca8499c92145f56.jpeg'           ,
+    '2023072514_f4be853beaa4552f2663043847646f71.png' ,
+    'image113.jpeg'                                   ,
+    '2023072514_1088abfc19133e3692a78a68d4e3b171.png' ,
+    'image139.jpeg'                                   ,
+    'image154.jpeg'                                   ,
+    'image24.png'                                     ,
+    '2023060616_d690ad24ae395bdf0e0ec50cd4fed442.jpeg',
+    '2023072514_ea5d08a4b98560a728174ee5c058ecb3.png' ,
+           ]
 for k in sorted(os.listdir(test_path)):
+# for k in imglist:
+    ocr_result_dict = {}
     img = cv2.imread(os.path.join(test_path,k))
+    if not isinstance(img,np.ndarray):
+        continue
     ocr_result_dict[k] = ocr.ocr(img)
-res = model.inference(ocr_result_dict)
-print(res)
+    res = model.inference(ocr_result_dict)
+    print(res)
     # if res[0] == 1:
     #     shutil.move(os.path.join(test_path,k),os.path.join('/home/topnet/图片/设备购置合同/head',k))
     # elif res[0] == 2:
     #     shutil.move(os.path.join(test_path,k),os.path.join('/home/topnet/图片/设备购置合同/tail',k))
-    # cv2.putText(img,str(res[0]),(100,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),5)
-    # cv2.imshow(k,img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    cv2.putText(img,str(res[0]),(100,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),5)
+    cv2.imshow(k,img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 """
 ocr_result_dict = {
 "img1.jpg":ocr.ocr(img1),
